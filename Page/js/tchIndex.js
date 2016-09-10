c4456eeac46e204c1fd47c8e30054a110b83508c
ChocolateYD <583759657@qq.com>
2016-09-10 10:07:57 +0800

    主文件夹

--- Page/js/tchIndex.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2016/9/7.
- */
-$(function(){
-    $('#tchIndexPronav div').click(function(){
-        $(this).css('background-image',"url('image/gengduo2.png')");
-        //$(this).css('background',"yellow");
-    })
-});
